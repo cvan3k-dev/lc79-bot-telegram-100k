626ddeb506b1b42a563fbfc38ff9bd828b003caf
cvan3k-dev <cvan3k@gmail.com>
2026-08-11 20:57:05 +0700

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
         asyncio.run(main())
     except RuntimeError as e:
         if "already running" in str(e):
-            # Tạo event loop mới
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
             try:
